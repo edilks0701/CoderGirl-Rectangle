01c3e28b9241c246e144b18eb4afe5c27b6ad1b3
Jonny McFadden <jmcfadden1528@gmail.com>
2019-03-03 16:35:27 -0600

    completed area of rectangle calculator

--- Rectangle/Program.cs
+++ Rectangle/Program.cs
@@ -7,15 +7,20 @@ namespace Rectangle
         public static void Main()
         {
             Console.Write("Enter the length of the rectangle: ");
-
+            string length = Console.ReadLine();
+            int parsedLength = int.Parse(length);
+            
             // Get input from the user and save it to a variable.
 
             Console.Write("Enter the width of the rectangle: ");
-
+            string width = Console.ReadLine();
+            int parsedWidth = int.Parse(width);
             // Get input from the user and save it to a second variable.
 
             // Output - The area of the rectangle is (computed value)
 
+            int area = (parsedWidth * parsedLength);
+            Console.WriteLine(area);
             Console.ReadLine();
         }
     }
